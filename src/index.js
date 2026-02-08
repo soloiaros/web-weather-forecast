ee4f62fdfb99af63d1b02abdf12d11822062a9d0
soloiaros <soloiaros@gmail.com>
2026-02-08 20:50:26 +0300

    Add weather display logic, fix layout Major layout problems, such as width overflow,  were fixed for the entire page. Basic loading animation was implemented. Weather in searched location is now being displayed upon fetching and stringifying (now only in terms of color of the widgets)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 import searchPlace from "./api-fetch";
-import './styles.css';
+import "./styles.css";
 
-const inputBox = document.querySelector('input');
-const btnSearch = document.querySelector('button');
+const inputBox = document.querySelector("input");
+const btnSearch = document.querySelector("button");
 
-btnSearch.addEventListener('click', () => {
+btnSearch.addEventListener("click", async function () {
   const requestedPlace = inputBox.value;
-  const weatherData = searchPlace(requestedPlace);
-  weatherData.then((result) => { console.log(result) })
-})
+  displaySkeletonWidgets();
+  const weatherData = await searchPlace(requestedPlace);
+  fillOutWidgets(weatherData);
+});
+
+const degreeSwitcher = document.getElementById("degree-switcher");
+degreeSwitcher.addEventListener("click", () => {
+  const toggleHead = document.getElementById("toggle-head");
+  if (degreeSwitcher.checked) {
+    toggleHead.classList = "right";
+  } else {
+    toggleHead.classList = "left";
+  }
+});
+
+function displaySkeletonWidgets() {
+  const widgets = document.querySelectorAll(".widget");
+  console.log(widgets);
+  for (let widget of widgets) {
+    widget.classList.add("skeleton");
+  }
+}
+
+function fillOutWidgets(weatherData) {
+  const widgets = document.querySelectorAll(".widget");
+  const possibleStates = ["skeleton", "sun", "rain", "overcast"];
+  for (let widget of widgets) {
+    const currentClasses = [...widget.classList];
+    for (let widClass of currentClasses) {
+      if (possibleStates.includes(widClass)) {
+        widget.classList.remove(widClass);
+      }
+    }
+    if (weatherData.today.icon === "rain") {
+      widget.classList.add("rain");
+    } else if (weatherData.today.icon === "sunny") {
+      widget.classList.add("sun");
+    } else {
+      widget.classList.add("overcast");
+    }
+  }
+}
